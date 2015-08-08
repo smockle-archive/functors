bf9be537c99e36598de7634c9ba85081c14bbed8
Clay Miller <clay@smockle.com>
2015-08-08 01:25:10 -0700

    Remove .of method. Add dotfiles.

--- tests/container.js
+++ tests/container.js
@@ -4,24 +4,24 @@ import util from 'util';
 import * as _ from 'ramda';
 
 describe('Container', () => {
-  describe('#of', () => {
+  describe('#constructor', () => {
     it('should return a Container of number', () => {
       assert.instanceOf(
-        Container.of(3),
+        Container(3),
         Container
       );
     });
 
     it('should return a Container of string', () => {
       assert.instanceOf(
-        Container.of('hotdogs'),
+        Container('hotdogs'),
         Container
       );
     });
 
     it('should return a Container of a Container of Object', () => {
       assert.instanceOf(
-        Container.of(Container.of({ name: 'yoda' })),
+        Container(Container({ name: 'yoda' })),
         Container
       );
     });
@@ -30,22 +30,22 @@ describe('Container', () => {
   describe('#inspect', () => {
     it('should output a Container of number', () => {
       assert.strictEqual(
-        util.inspect(Container.of(3)),
-        'Container.of(3)'
+        util.inspect(Container(3)),
+        'Container(3)'
       );
     });
 
     it('should output a Container of string', () => {
       assert.strictEqual(
-        util.inspect(Container.of('hotdogs')),
-        'Container.of(\'hotdogs\')'
+        util.inspect(Container('hotdogs')),
+        'Container(\'hotdogs\')'
       );
     });
 
     it('should output a Container of a Container of Object', () => {
       assert.strictEqual(
-        util.inspect(Container.of(Container.of({ name: 'yoda' }))),
-        'Container.of(Container.of({"name":"yoda"}))'
+        util.inspect(Container(Container({ name: 'yoda' }))),
+        'Container(Container({"name":"yoda"}))'
       );
     });
   });
@@ -53,22 +53,22 @@ describe('Container', () => {
   describe('#map', () => {
     it('should map to a Container of number', () => {
       assert.deepEqual(
-        Container.of(2).map(function(two) { return two + 2 }),
-        Container.of(4)
+        Container(2).map(function(two) { return two + 2 }),
+        Container(4)
       );
     });
 
     it('should map to a Container of string', () => {
       assert.deepEqual(
-        Container.of("flamethrowers").map(function(s) { return s.toUpperCase() }),
-        Container.of('FLAMETHROWERS')
+        Container("flamethrowers").map(function(s) { return s.toUpperCase() }),
+        Container('FLAMETHROWERS')
       );
     });
 
     it('should chain successive #map', () => {
       assert.deepEqual(
-        Container.of("bombs").map(_.concat(' away')).map(_.prop('length')),
-        Container.of(10)
+        Container("bombs").map(_.concat(' away')).map(_.prop('length')),
+        Container(10)
       );
     });
   });
--- tests/maybe.js
+++ tests/maybe.js
@@ -4,24 +4,24 @@ import util from 'util';
 import * as _ from 'ramda';
 
 describe('Maybe', () => {
-  describe('#of', () => {
+  describe('#constructor', () => {
     it('should return a Maybe of number', () => {
       assert.instanceOf(
-        Maybe.of(3),
+        Maybe(3),
         Maybe
       );
     });
 
     it('should return a Maybe of string', () => {
       assert.instanceOf(
-        Maybe.of('hotdogs'),
+        Maybe('hotdogs'),
         Maybe
       );
     });
 
     it('should return a Maybe of a Maybe of Object', () => {
       assert.instanceOf(
-        Maybe.of(Maybe.of({ name: 'yoda' })),
+        Maybe(Maybe({ name: 'yoda' })),
         Maybe
       );
     });
@@ -30,59 +30,59 @@ describe('Maybe', () => {
   describe('#inspect', () => {
     it('should output a Maybe of number', () => {
       assert.strictEqual(
-        util.inspect(Maybe.of(3)),
-        'Maybe.of(3)'
+        util.inspect(Maybe(3)),
+        'Maybe(3)'
       );
     });
 
     it('should output a Maybe of string', () => {
       assert.strictEqual(
-        util.inspect(Maybe.of('hotdogs')),
-        'Maybe.of(\'hotdogs\')'
+        util.inspect(Maybe('hotdogs')),
+        'Maybe(\'hotdogs\')'
       );
     });
 
     it('should output a Maybe of a Maybe of Object', () => {
       assert.strictEqual(
-        util.inspect(Maybe.of(Maybe.of({ name: 'yoda' }))),
-        'Maybe.of(Maybe.of({"name":"yoda"}))'
+        util.inspect(Maybe(Maybe({ name: 'yoda' }))),
+        'Maybe(Maybe({"name":"yoda"}))'
       );
     });
   });
 
   describe('#isNothing', () => {
     it('should return true for null value', () => {
-      assert.isTrue(Maybe.of(null).isNothing());
+      assert.isTrue(Maybe(null).isNothing());
     });
 
     it('should return true for undefined value', () => {
-      assert.isTrue(Maybe.of(undefined).isNothing());
+      assert.isTrue(Maybe(undefined).isNothing());
     });
 
     it('should return false for non-null value', () => {
-      assert.isFalse(Maybe.of(3).isNothing());
+      assert.isFalse(Maybe(3).isNothing());
     });
   });
 
   describe('#map', () => {
     it('should map to a Maybe of number', () => {
       assert.deepEqual(
-        Maybe.of(2).map(function(two) { return two + 2 }),
-        Maybe.of(4)
+        Maybe(2).map(function(two) { return two + 2 }),
+        Maybe(4)
       );
     });
 
     it('should map to a Maybe of string', () => {
       assert.deepEqual(
-        Maybe.of("flamethrowers").map(function(s) { return s.toUpperCase() }),
-        Maybe.of('FLAMETHROWERS')
+        Maybe("flamethrowers").map(function(s) { return s.toUpperCase() }),
+        Maybe('FLAMETHROWERS')
       );
     });
 
     it('should chain successive #map', () => {
       assert.deepEqual(
-        Maybe.of("bombs").map(_.concat(' away')).map(_.prop('length')),
-        Maybe.of(10)
+        Maybe("bombs").map(_.concat(' away')).map(_.prop('length')),
+        Maybe(10)
       );
     });
   });
